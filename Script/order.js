df870c4108551099e7a6462145ade6f3feb0a1c5
Novika Rosellini <rosellou.novika12@gmail.com>
2022-01-08 17:52:07 +0700

    order

--- Script/order.js
+++ Script/order.js
@@ -15,36 +15,71 @@ function validate() {
 
     // Name validation
     var x = document.getElementById("fname");
-    if (x.value == "" || x.value == null) {
+    var titleLength = 0;
+    //Jenis validation 1: valueOf()
+    if (x.value.valueOf() == "" || x.value.valueOf() == null) {
         // alert( "Please provide your name!" )
         document.getElementById('errorName').innerHTML = "Please input your name";
         x.classList.add("errorInput");
         x.focus();
         return false;
     }
-    else if (x.value.length < 3 || x.value.length > 25) {
-        // alert( "Please provide your name!" )
-        document.getElementById('errorName').innerHTML = "Please input 3 to 25 characters";
+    //Jenis validation 2: startsWith()
+    else if (!x.value.startsWith("Mr. ") && !x.value.startsWith("Mrs. ") && !x.value.startsWith("Ms. ")){
+        document.getElementById('errorName').innerHTML = "Please input your titles \"Mr. /Mrs. /Ms. \" (use space)";
         x.classList.add("errorInput");
         x.focus();
         return false;
+    }else{
+        titleLength = x.value.search(" ");
+        //Jenis validation 3: length
+        if (x.value.length-(titleLength+1) < 5 || x.value.length-(titleLength+1) > 30) {
+            // alert( "Please provide your name!" )
+            document.getElementById('errorName').innerHTML = "Please input 5 to 30 name characters";
+            x.classList.add("errorInput");
+            x.focus();
+            return false;
+        }
+        //Jenis validation 4&5: lastIndexOf(), endsWith
+        else if(x.value.lastIndexOf(" ")==titleLength || x.value.endsWith(" ")){
+            document.getElementById('errorName').innerHTML = "Please input at least 2 words of name";
+            x.classList.add("errorInput");
+            x.focus();
+            return false;
+        }
     }
+    
+    
 
     // Email validation
     x = document.getElementById("email");
-    if (x.value == "" || x.value == null) {
+    if (x.value.valueOf() == "" || x.value.valueOf() == null) {
         // alert( "Please provide your emai!" )
         document.getElementById('errorEmail').innerHTML = "Please input your email";
         x.classList.add("errorInput");
         x.focus();
         return false;
     }
+    //Jenis validation 6: includes()
+    else if(!x.value.includes("@")){
+        document.getElementById('errorEmail').innerHTML = "Email must contain \"@\" character";
+            x.classList.add("errorInput");
+            x.focus();
+            return false;
+    }
+    //endsWith
+    else if(!x.value.endsWith(".com")&&!x.value.endsWith(".id")){
+        document.getElementById('errorEmail').innerHTML = "Email must end with \".com\" or \".id\"";
+            x.classList.add("errorInput");
+            x.focus();
+            return false;
+    }
     else {
+        //Jenis validation 7: indexOf()
         var at = x.value.indexOf("@");
         var lastAt = x.value.indexOf("@");
         var dot = x.value.indexOf(".");
         var lastDot = x.value.lastIndexOf(".");
-        // console.log(at + ", "+ dot);
 
         if (at < 1 || dot - at < 2 || lastDot == x.value.length - 1 || at != lastAt) {
             document.getElementById('errorEmail').innerHTML = "Please input valid email";
@@ -59,7 +94,7 @@ function validate() {
     var y = document.getElementsByClassName('errorMsg errorQty');
     var total = 0;
     for (i = 0; i < x.length; i++) {
-        if (x[i].value == "") {
+        if (x[i].value.valueOf() == "") {
             total+=0;
         }
         else if (x[i].value < 1 || x[i].value > 10) {
@@ -87,7 +122,7 @@ function validate() {
     var qty = document.getElementsByClassName('Qty');
     y = document.getElementsByClassName('errorMsg errorQty');
     var totalBurger=0;
-    if(x.value == "2mini"){
+    if(x.value.valueOf() == "2mini"){
 
         if(qty[6].value<2){
             y[6].innerHTML = "Please input at least 2 quantity to redeem the promo";
@@ -96,7 +131,7 @@ function validate() {
             return false;
         }
     }
-    else if(x.value == "burgercola"){
+    else if(x.value.valueOf() == "burgercola"){
         for (i = 0; i < 6; i++) {
             totalBurger += qty[i].value;
         }
@@ -131,16 +166,16 @@ function validate() {
 
     // Address Validation
     x = document.getElementById("address");
-    if (x.value == "" || x.value == null) {
+    if (x.value.valueOf() == "" || x.value.valueOf() == null) {
         // alert( "Please provide your emai!" )
         document.getElementById('errorAddress').innerHTML = "Please input your address";
         x.classList.add("errorInput");
         x.focus();
         return false;
     }
-    else if (x.value.length < 3 || x.value.length > 50) {
+    else if (x.value.length < 5 || x.value.length > 50) {
         // alert( "Please provide your name!" )
-        document.getElementById('errorAddress').innerHTML = "Please input 3 to 50 characters";
+        document.getElementById('errorAddress').innerHTML = "Please input 5 to 50 characters";
         x.classList.add("errorInput");
         x.focus();
         return false;
